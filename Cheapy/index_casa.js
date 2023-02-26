aef7a2cd3b26df71e38992cc51e8f99d394101fd
username <margiecagal@gmail.com>
2023-02-25 18:15:44 -0800

    yes

---
+++ Cheapy/index_casa.js
@@ -0,0 +1,22 @@
+fetch("./casa.json")
+    .then(function(response) {
+        return response;
+    })
+    .then(function(data) {
+        return casa.json();
+    })
+    .then(function (Normal) {
+        const html = Normal.map(
+            (data) => `<div class="box" id="naturalhistorymuseum">
+            <img src="Natural-History-Museum-Logo.png">
+            <h1>${data.productservice}</h1>
+            <p class="location" id="location1">900 Exposition Blvd, Los Angeles, CA 90007</p>
+            <p class="discount" id="discount1"></p>
+        </div> `
+        );
+        document.getElementById("maincontainer").innerHTML = html;
+    })
+    .catch(function(err) {
+        console.log("Fetch problem show: " + err.message);
+    }
+    );
